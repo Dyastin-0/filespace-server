7ba3720f4a8fbabcf353a2b6c56d2a2c04120a8b
Justine <paralejasjustine15@gmail.com>
2024-12-24 18:33:03 +0800

    Modified build script.

--- build.sh
+++ build.sh
@@ -2,56 +2,71 @@
 
 OUTPUT_DIR="/opt/filespace"
 MAIN_PACKAGE="./cmd/server/main.go"
-ENV_PATH="./.env"
 BINARY_NAME="run"
+ENV_FILE="./.env"
+ENV_PATH=$OUTPUT_DIR/.env
+SECRET_FILE="./secretsaccesor.json"
+SECRET_PATH=$OUTPUT_DIR/secretsaccesor.json
 SERVICE_FILE="filespace-v2.service"
 SERVICE_PATH="/etc/systemd/system/$SERVICE_FILE"
 
+copy_file() {
+    local source_file=$1
+    local dest_file=$2
+    echo "Copying $source_file to $dest_file..."
+    sudo cp "$source_file" "$dest_file"
+    if [ $? -eq 0 ]; then
+        echo "[Filespace] $source_file successfully moved to $dest_file"
+    else
+        echo "Failed to move $source_file. Check permissions or path."
+        exit 1
+    fi
+}
+
+echo "Moving $SERVICE_FILE to $SERVICE_PATH..."
+copy_file ./$SERVICE_FILE $SERVICE_PATH
+
 sudo mkdir -p $OUTPUT_DIR
 
-echo "Building the binary..."
-sudo go build -ldflags="-s -w" -o $OUTPUT_DIR/$BINARY_NAME $MAIN_PACKAGE
-
+echo "[Filespace] Building the binary..."
+go build -ldflags="-s -w" -o $OUTPUT_DIR/$BINARY_NAME $MAIN_PACKAGE
 if [ $? -eq 0 ]; then
-    echo "Build successful. Binary located at $OUTPUT_DIR/$BINARY_NAME"
+    echo "[Filespace] Build successful. Binary located at $OUTPUT_DIR/$BINARY_NAME"
 else
-    echo "Build failed. Check errors above."
-	exit 1
+    echo "[Filespace] Build failed. Check errors above."
+    exit 1
 fi
 
-if [ -f "./$SERVICE_FILE" ]; then
-	echo "Moving $SERVICE_FILE to $SERVICE_PATH..."
-	sudo cp ./$SERVICE_FILE $SERVICE_PATH
-
-	echo "Generating .env file..."
-	if go run ./pkg/secret/secret.go; then
-		echo ".env file generated successfully."
-	else
-		echo "Failed to generate .env file."
-		exit 1
-	fi
-
-	echo "Moving .env file to $OUTPUT_DIR..."
-	sudo cp $ENV_PATH $OUTPUT_DIR/.env
-
-	echo "Moving secret acccesor to $OUTPUT_DIR..."
-	sudo cp ./secretsaccesor.json $OUTPUT_DIR/secretsaccesor.json
-
-	if [ $? -eq 0 ]; then
-		echo "Service file moved successfully to $SERVICE_PATH."
-		sudo systemctl daemon-reload
-		if systemctl is-active --quiet $SERVICE_FILE; then
-			sudo systemctl restart $SERVICE_FILE
-		else
-			sudo systemctl start $SERVICE_FILE
-		fi
-		sudo systemctl restart caddy
-		echo "Service started and enabled."
-	else
-		echo "Failed to move service file. Check permissions or path."
-		exit 1
-	fi
+echo "Generating .env file..."
+if ! go run ./pkg/secret/secret.go; then
+    echo "Failed to generate .env file"
+    exit 1
 else
-	echo "Service file $SERVICE_FILE not found in the root directory."
-	exit 1
+    echo ".env file generated successfully"
 fi
+
+copy_file $ENV_FILE $ENV_PATH
+copy_file $SECRET_FILE $SECRET_PATH
+
+echo "[Filespace] Reloading systemd daemon..."
+sudo systemctl daemon-reload
+
+if systemctl is-active --quiet $SERVICE_FILE; then
+	echo "[Filespace] Restarting the service..."
+    sudo systemctl restart $SERVICE_FILE
+	echo "[Filespace] Service restarted"
+else
+	echo "[Filespace] Starting the service..."
+    sudo systemctl start $SERVICE_FILE
+	echo "[Filespace] Service started"
+fi
+
+sudo systemctl restart caddy
+echo "[Filespace] Caddy restarted"
+
+echo "[Filespace] Service build and deployment complete"
+ 
+#  The script is pretty straightforward. It builds the binary, copies the necessary files to the output directory, generates the  .env  file, and moves the service file to the systemd directory. 
+#  The script also restarts the service and Caddy server. 
+#  To run the script, execute the following command: 
+#  chmod +x build.sh
