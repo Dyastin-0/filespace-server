27963d9479a2291b5311185beb5a319ebf0c67db
Justine <paralejasjustine15@gmail.com>
2024-12-24 15:30:48 +0800

    Added build and service config file.

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 OUTPUT_DIR="/opt/filespace"
 MAIN_PACKAGE="./cmd/server/main.go"
-BINARY_NAME="filespace"
+ENV_PATH="./.env"
+BINARY_NAME="run"
+SERVICE_FILE="filespace.service"
+SERVICE_PATH="/etc/systemd/system/$SERVICE_FILE"
 
 mkdir -p $OUTPUT_DIR
 
@@ -10,8 +13,38 @@ echo "Building the application..."
 go build -ldflags="-s -w" -o $OUTPUT_DIR/$BINARY_NAME $MAIN_PACKAGE
 
 if [ $? -eq 0 ]; then
-  echo "Build successful. Binary located at $OUTPUT_DIR/$BINARY_NAME"
+    echo "Build successful. Binary located at $OUTPUT_DIR/$BINARY_NAME"
 else
-  echo "Build failed. Check errors above."
-  exit 1
+    echo "Build failed. Check errors above."
+	exit 1
+fi
+
+if [ -f "./$SERVICE_FILE" ]; then
+	echo "Moving $SERVICE_FILE to $SERVICE_PATH..."
+	sudo cp ./$SERVICE_FILE $SERVICE_PATH
+
+	echo "Generating .env file..."
+	if go run ./pkg/secret/secret.go; then
+		echo ".env file generated successfully."
+	else
+		echo "Failed to generate .env file."
+		exit 1
+	fi
+
+	echo "Moving .env file to $OUTPUT_DIR..."
+	sudo cp $ENV_PATH $OUTPUT_DIR/.env
+
+	if [ $? -eq 0 ]; then
+		echo "Service file moved successfully to $SERVICE_PATH."
+		sudo systemctl daemon-reload
+		sudo systemctl enable $SERVICE_FILE --now 
+		sudo systemctl restart caddy
+		echo "Service started and enabled."
+	else
+		echo "Failed to move service file. Check permissions or path."
+		exit 1
+	fi
+else
+	echo "Service file $SERVICE_FILE not found in the root directory."
+	exit 1
 fi
